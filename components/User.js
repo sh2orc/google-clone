cf20a2d2b1d4926f7d0265f1ccd38c9f91711c5a
Lee Taeyoung <seen@korea.ac.kr>
2022-06-01 23:22:32 +0900

    add authentication and get user data from google

--- components/User.js
+++ components/User.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import {useSession, signIn, signOut} from "next-auth/react";
 
 function User(props) {
+    const {data : session} = useSession()
+
+    if(session){
+        return (
+            <>
+                <img onClick={signOut} src={session.user.image} className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"/>
+            </>
+        )
+    }
+
     return (
-        <div>User</div>
+        <>
+            <button onClick={signIn} className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:shadow-md hover:brightness-105">Sign in</button>
+        </>
     );
 }
 
