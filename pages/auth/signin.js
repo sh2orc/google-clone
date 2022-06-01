b2da8f8810d2c09f264be04b7152046a2a28c42b
Lee Taeyoung <seen@korea.ac.kr>
2022-06-01 23:22:46 +0900

    add authentication and get user data from google

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -7,7 +7,7 @@ function signin({providers}) {
     return (
         <>
             <Header/>
-            <div class="mt-40">
+            <div className="mt-40">
                 {Object.values(providers).map(provider =>(
                     <div key={provider.name} className="flex flex-col items-center">
                         <img className="w-52 object-cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="google logo"/>
