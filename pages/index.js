0e04525879c8f5efba528240a0c45e3c4c550de1
Lee Taeyoung <seen@korea.ac.kr>
2022-06-02 19:31:34 +0900

    get term from input and redirect uri

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,22 @@ import Header from "../components/Header";
 import Image from "next/image";
 import {MicrophoneIcon, SearchIcon} from "@heroicons/react/solid";
 import Footer from "../components/Footer";
+import {useRouter} from "next/router";
+import {useRef} from "react";
 
 export default function Home() {
-  return (
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  function search(event){
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+    if(!term) return;
+
+    router.push(`/search?term=${term.trim()}`);
+  }
+
+    return (
     <div>
     <Head>
         <title>Google V3</title>
@@ -24,12 +37,12 @@ export default function Home() {
             height={100}
             src={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"}/>
         <div className="flex w-full justify-center mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
-            <SearchIcon className="h-5 text-gray-500"/>
-            <input type="text" className={"flex-grow focus:outline-none"}/>
+            <SearchIcon  className="h-5 text-gray-500"/>
+            <input ref={searchInputRef} type="text" className={"flex-grow focus:outline-none"}/>
             <MicrophoneIcon className="h-5  text-gray-500"/>
         </div>
         <div className={"flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 items-center justify-center"}>
-            <button className={"btn"}>Google Search</button>
+            <button onClick={search} className={"btn"}>Google Search</button>
             <button className={"btn"}>I'm Feeling Lucky</button>
         </div>
     </form>
