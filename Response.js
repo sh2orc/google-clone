5a2e4240e7cb496760fcf9571894549adb4b0c1f
Lee Taeyoung <seen@korea.ac.kr>
2022-06-03 23:20:49 +0900

    get data from google api and create the mock data

---
+++ Response.js
@@ -0,0 +1,20 @@
+export default {
+    "kind": "customsearch#result",
+    "title": "What Is Windows Hello Sign-in on Windows 10?",
+    "htmlTitle": "What Is Windows <b>Hello</b> Sign-in on Windows 10?",
+    "link": "https://www.howtogeek.com/wp-content/uploads/2021/01/windows_hello_hero_2.jpg?height=200p&trim=2,2,2,2&crop=16:9",
+    "displayLink": "www.howtogeek.com",
+    "snippet": "What Is Windows Hello Sign-in on Windows 10?",
+    "htmlSnippet": "What Is Windows <b>Hello</b> Sign-in on Windows 10?",
+    "mime": "image/jpeg",
+    "fileFormat": "image/jpeg",
+    "image": {
+        "contextLink": "https://www.howtogeek.com/708986/what-is-windows-hello-sign-in-on-windows-10/",
+        "height": 592,
+        "width": 1052,
+        "byteSize": 18702,
+        "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS04Juf3ty9KoPB-mkmbsE1ePUldn4KYTSO5L0SFn6_N2NHs0ljpE855tI&s",
+        "thumbnailHeight": 84,
+        "thumbnailWidth": 150
+    }
+}
--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-function Search(props) {
+function Search({results}) {
+
+    console.log(results);
     return (
         <div>
             <Head>
@@ -16,3 +19,17 @@ function Search(props) {
 }
 
 export default Search;
+
+export async function getServerSideProps(context){
+
+    const mockData = true;
+
+    const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`)
+                    .then((response)=> response.json())
+        ;
+
+    return { props: {
+            results : data
+        }
+    };
+}
