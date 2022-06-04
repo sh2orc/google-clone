b69c6be63f3f1e2e9a9a83ecb29720e3230673cd
Lee Taeyoung <seen@korea.ac.kr>
2022-06-04 11:56:23 +0900

    complete the result section and install html react parser

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Parser from 'html-react-parser';
 
 function SearchResult({results}) {
     return (
         <div className={"w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52"}>
             <p className={"text-gray-600 text-sm mb-5 mt-3"}>About {results.searchInformation.formattedTotalResults} results ({results.searchInformation.formattedSearchTime} seconds)</p>
+
+            {results.items.map(result =>(
+                <div key={result.link} className={"max-w-xl mb-8"}>
+                    <div className={"group"}>
+                        <a className={"text-sm truncate"} href={result.link}>{result.displayLink}</a>
+                        <a className={"group-hover:underline decoration-blue-800 underline-offset-4"} href={result.link}>
+                            <h2 className={"truncate text-xl font-medium text-blue-800"}>{result.title}</h2></a>
+                    </div>
+                    <p>{Parser(result.htmlSnippet)}</p>
+                </div>
+                ))}
+
         </div>
     );
 }
--- pages/search.js
+++ pages/search.js
@@ -30,7 +30,8 @@ export async function getServerSideProps(context){
 
     const mockData = false;
 
-    const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`)
+    const data = mockData ? Response : await
+                    fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`)
                     .then((response)=> response.json())
         ;
 
